IlyaSkriblovsky/FlowchartBuilder

commit 99a5d71ecd9680c4a12bfbe910a93cc2dd37f28e

Stronger typing using TList<> (requires Delphi 2009+)

Source/EdTypes.pas
@@ -49,8 +49,7 @@ var
 begin
   Result := false;
   for i := 0 to ChildForm.ArrowList.Count - 1 do
-    if (TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Block = Self) and
-      (TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Port = South) then
+    if (ChildForm.ArrowList.Items[i].Blocks[atEnd].Block = Self) and (ChildForm.ArrowList.Items[i].Blocks[atEnd].Port = South) then
       Result := true;
 end;
 
@@ -58,6 +57,7 @@ function TBlock.IsPortAvail(t: TArrowTail; p: TBlockPort; laa: boolean): boolean
 var
   i: integer;
   w, s, e: bool;
+  blockRec: TBlockRec;
 
 begin
   if Block <> stIf then
@@ -76,13 +76,14 @@ begin
         s := false;
         e := false;
         for i := 0 to ChildForm.ArrowList.Count - 1 do
-          if TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Block = Self then
+          blockRec := ChildForm.ArrowList.Items[i].Blocks[atEnd];
+          if blockRec.Block = Self then
           begin
-            if TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Port = West then
+            if blockRec.Port = West then
               w := true;
-            if TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Port = South then
+            if blockRec.Port = South then
               s := true;
-            if TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Port = East then
+            if blockRec.Port = East then
               e := true;
           end;
         if (w and s) or (s and e) or (e and w) then

Source/Arrows.pas
@@ -73,7 +73,6 @@ type
   public
     Ins: set of TBlockPort;
     Blocked: set of TBlockPort;
-    Tag1: integer; // may be usable
 
     procedure DrawPort(p: TBlockPort); virtual; abstract;
 
@@ -227,8 +226,8 @@ begin
       Result := false;
     if LookAtAlready then
       for i := 0 to ChildForm.ArrowList.Count - 1 do
-        if (TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Block = Self) and
-          (TArrow(ChildForm.ArrowList[i]).Blocks[atEnd].Port = p) then
+        if (ChildForm.ArrowList.Items[i].Blocks[atEnd].Block = Self) and
+          (ChildForm.ArrowList.Items[i].Blocks[atEnd].Port = p) then
           Result := false;
   end;
 end;

Source/Editor.dpr
@@ -1,4 +1,4 @@
-program Editor;
+п»їprogram Editor;
 
 uses
   Windows,
@@ -6,31 +6,31 @@ uses
   Classes,
   SysUtils,
   Dialogs,
-  Main in 'Main.pas' {MainForm},
+  Generics.Collections,
+  Main in 'Main.pas' {MainForm} ,
   EdTypes in 'EdTypes.pas',
-  About in 'About.pas' {AboutBox},
-  OutProg in 'OutProg.pas' {frmOutProg},
-  Options in 'Options.pas' {frmOpt},
+  About in 'About.pas' {AboutBox} ,
+  OutProg in 'OutProg.pas' {frmOutProg} ,
+  Options in 'Options.pas' {frmOpt} ,
   OpenUnit in 'OpenUnit.pas',
   SaveUnit in 'SaveUnit.pas',
-  Child in 'Child.pas' {ChildForm},
-  Watch in 'Watch.pas' {frmWatch},
+  Child in 'Child.pas' {ChildForm} ,
+  Watch in 'Watch.pas' {frmWatch} ,
   Lang in 'Lang.pas',
-  uInterval in 'uInterval.pas' {frmInterval},
+  uInterval in 'uInterval.pas' {frmInterval} ,
   Arrows in 'Arrows.pas',
-  StrsInput in 'StrsInput.pas' {frmStrsForm},
+  StrsInput in 'StrsInput.pas' {frmStrsForm} ,
   ini in 'ini.pas',
-  BlockProps in 'BlockProps.pas' {frmProps},
+  BlockProps in 'BlockProps.pas' {frmProps} ,
   Version in 'Version.pas',
-  ZoomForm in 'ZoomForm.pas' {frmZoom};
-
+  ZoomForm in 'ZoomForm.pas' {frmZoom} ;
 {$R *.RES}
 
 begin
-  AplName:=ParamStr(0);
-  MyDir:=ExtractFileDir(ParamStr(0))+'\';
+  AplName := ParamStr(0);
+  MyDir := ExtractFileDir(ParamStr(0)) + '\';
   Application.Initialize;
-  Application.Title:='Конструктор блок-схем';
+  Application.Title := 'РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ Р±Р»РѕРє-СЃС…РµРј';
   Application.CreateForm(TMainForm, MainForm);
   Application.CreateForm(TAboutBox, AboutBox);
   Application.CreateForm(TfrmInterval, frmInterval);
@@ -41,90 +41,92 @@ begin
   Application.CreateForm(TfrmOpt, frmOpt);
   Application.CreateForm(TfrmWatch, frmWatch);
   Application.CreateForm(TfrmZoom, frmZoom);
-  if Viewer
-  then begin
-         (***  MainForm  ***)
-         with MainForm
-         do begin
-              Caption:='Интерпретатор блок-схем';
-              Application.Title:='Интерпретатор блок-схем';
+  if Viewer then
+  begin
+    (* **  MainForm  ** *)
+    with MainForm do
+    begin
+      Caption := 'РРЅС‚РµСЂРїСЂРµС‚Р°С‚РѕСЂ Р±Р»РѕРє-СЃС…РµРј';
+      Application.Title := 'РРЅС‚РµСЂРїСЂРµС‚Р°С‚РѕСЂ Р±Р»РѕРє-СЃС…РµРј';
 
-           (***  Hiding Unnecessary ;o)  ***)
-              btnNew.Visible:=false;
-              if btnBrowser.Visible
-              then tbrFile.Width:=btnOpen.Width+btnSave.Width+btnBrowser.Width
-              else tbrFile.Width:=btnOpen.Width+btnSave.Width;
-              tbrBlocks.Visible:=false;
-              tbrAlign.Visible:=false;
-              btnLineRun.Visible:=false;
-              btnDelete.Visible:=false;
-              mnuEdit.Visible:=false;
-              mnuEdit.Enabled:=false;
-              mnuNew.Visible:=false;
+      (* **  Hiding Unnecessary ;o)  ** *)
+      btnNew.Visible := false;
+      if btnBrowser.Visible then
+        tbrFile.Width := btnOpen.Width + btnSave.Width + btnBrowser.Width
+      else
+        tbrFile.Width := btnOpen.Width + btnSave.Width;
+      tbrBlocks.Visible := false;
+      tbrAlign.Visible := false;
+      btnLineRun.Visible := false;
+      btnDelete.Visible := false;
+      mnuEdit.Visible := false;
+      mnuEdit.Enabled := false;
+      mnuNew.Visible := false;
 
-           (***  Aligning toolbars  ***)
-              tbrFile.Left:=0;
-              tbrDebug.Left:=tbrFile.Width-10;
-              tbrView.Left:=tbrDebug.Left+tbrDebug.Width-10;
-              btnSettings.Left:=tbrView.Left+tbrView.Width+10;
-              btnZoom.Left:=btnSettings.Left+btnSettings.Width;
-            end;
+      (* **  Aligning toolbars  ** *)
+      tbrFile.Left := 0;
+      tbrDebug.Left := tbrFile.Width - 10;
+      tbrView.Left := tbrDebug.Left + tbrDebug.Width - 10;
+      btnSettings.Left := tbrView.Left + tbrView.Width + 10;
+      btnZoom.Left := btnSettings.Left + btnSettings.Width;
+    end;
 
-         (***  ChildForm  ***)
-         with ChildForm
-         do begin
-              mnuDelete.Visible:=false;
-              mnuReplace.Visible:=false;
-            end;
+    (* **  ChildForm  ** *)
+    with ChildForm do
+    begin
+      mnuDelete.Visible := false;
+      mnuReplace.Visible := false;
+    end;
 
-         (***  frmOpt  ***)
-         with frmOpt
-         do begin
-{              grpI13r.Visible:=false;
-              Shape2.Height:=233;
-              Memo2.Height:=96;
-              Height:=312;}
-              clbInterpr.Enabled:=false;
-            end;
+    (* **  frmOpt  ** *)
+    with frmOpt do
+    begin
+      { grpI13r.Visible:=false;
+        Shape2.Height:=233;
+        Memo2.Height:=96;
+        Height:=312; }
+      clbInterpr.Enabled := false;
+    end;
 
-         (***  frmProps  ***)
-         with frmProps
-         do begin
-              OpMemo.ReadOnly:=true;
-              TxMemo.ReadOnly:=true;
-              RmEdit.ReadOnly:=true;
-            end;
+    (* **  frmProps  ** *)
+    with frmProps do
+    begin
+      OpMemo.ReadOnly := true;
+      TxMemo.ReadOnly := true;
+      RmEdit.ReadOnly := true;
+    end;
 
-         (***  StrsForm  ***)
-         with StrsForm
-         do begin
-              Memo.ReadOnly:=true;
-            end;
+    (* **  StrsForm  ** *)
+    with StrsForm do
+    begin
+      Memo.ReadOnly := true;
+    end;
 
-         (***  AboutBox  ***)
-         with AboutBox
-         do begin
-              lblConstructor.Caption:='Интерпретатор блок-схем';
-            end;
-       end;
+    (* **  AboutBox  ** *)
+    with AboutBox do
+    begin
+      lblConstructor.Caption := 'РРЅС‚РµСЂРїСЂРµС‚Р°С‚РѕСЂ Р±Р»РѕРє-СЃС…РµРј';
+    end;
+  end;
 
-  MainForm.Modifed:=false;
-//////////////////////////////////////////////////////////////////
-//  if not Viewer
-//  then Registration;
-//////////////////////////////////////////////////////////////////
+  MainForm.Modifed := false;
+  /// ///////////////////////////////////////////////////////////////
+  // if not Viewer
+  // then Registration;
+  /// ///////////////////////////////////////////////////////////////
   if FileExists(ParamStr(1)) then
   begin
-    ChildForm.StartBlok:=nil;
-    ChildForm.FileName:=ParamStr(1);
+    ChildForm.StartBlok := nil;
+    ChildForm.FileName := ParamStr(1);
     ChildForm.DestroyList;
     ChildForm.RePaint;
-    ChildForm.BlockList:=TList.Create;
-    ChildForm.Dragging:=False;
-    ChildForm.flagInWork:=False;
+    ChildForm.BlockList := TList<TBlock>.Create;
+    ChildForm.Dragging := false;
+    ChildForm.flagInWork := false;
     LoadScheme(ChildForm.FileName);
-    ChildForm.Caption:=ParamStr(1);
+    ChildForm.Caption := ParamStr(1);
     ChildForm.SetRange;
   end;
   Application.Run;
+
 end.

Source/Lang.pas
@@ -39,7 +39,7 @@ unit Lang;
 
 interface
 
-uses SysUtils, Classes;
+uses SysUtils, Classes, Generics.Collections;
 
 const
   LexMax = 1000; // Max число лексем
@@ -60,16 +60,17 @@ type
     Real: TReal;
   end;
 
+  PValue = ^TValue;
+
   TVar = record
     Name: string;
     Value: TValue;
-    Sizes: TList;
-    Arr: TList;
+    Sizes: TList<PInteger>;
+    Arr: TList<PValue>;
   end;
 
   PVar = ^TVar;
-  PValue = ^TValue;
-  TVars = TList;
+  TVars = TList<PVar>;
 
   TLexeme = (lxUndef { # } , lxName { a } , lxNumber { 10 } , lxPlus { + } , lxMinus { - } , lxAsterisk { * } , lxSlash
     { / } , lxPower { ^ } , lxLBracket { ( } , lxRBracket { ) } , lxDiv { div } , lxMod { mod } , lxNot { not } , lxAnd
@@ -95,7 +96,7 @@ type
   ERunTimeError = class(Exception)
   end;
 
-  TGetFuncProc = function(Vars: TVars; Name: string; Params: TList): TValue;
+  TGetFuncProc = function(Vars: TVars; Name: string; Params: TList<PValue>): TValue;
 
   PFile_Rec = ^TFile_Rec;
 
@@ -119,7 +120,7 @@ var
   Pos: Cardinal;
   GetFuncValue: TGetFuncProc;
 
-  Files: TList;
+  Files: TList<PFile_Rec>;
   CurFile: integer = 0;
 
 procedure ReadBlock(ALines: TStrings; Lexemes: PLexemes);
@@ -127,8 +128,8 @@ procedure ReadBlock(ALines: TStrings; Lexemes: PLexemes);
 function CheckExpr(Lexemes: PLexemes): boolean;
 function ExecExpr(Lexemes: PLexemes; Vars: TVars): TValue;
 
-function GetVarValue(Vars: TVars; Name: string; ArrIndex: TList): TValue;
-procedure SetVarValue(Vars: TVars; Name: string; ArrIndex: TList; Value: TValue);
+function GetVarValue(Vars: TVars; Name: string; ArrIndex: TList<PInteger>): TValue;
+procedure SetVarValue(Vars: TVars; Name: string; ArrIndex: TList<PInteger>; Value: TValue);
 
 function CheckOperator(Lexemes: PLexemes): boolean;
 procedure ExecOperator(Lexemes: PLexemes; Vars: TVars);
@@ -137,7 +138,7 @@ procedure RunTimeError(Error: string);
 
 function GetVarIndex(Vars: TVars; Name: string): integer;
 
-function GetFuncResult(Vars: TVars; Name: string; Params: TList): TValue;
+function GetFuncResult(Vars: TVars; Name: string; Params: TList<PValue>): TValue;
 
 implementation
 
@@ -407,7 +408,7 @@ begin
   AlreadyError := true;
 end;
 
-function GetVarValue(Vars: TVars; Name: string; ArrIndex: TList): TValue;
+function GetVarValue(Vars: TVars; Name: string; ArrIndex: TList<PInteger>): TValue;
 var
   i, VarNo: LongInt;
   PlainNo: Cardinal;
@@ -417,7 +418,7 @@ var
 
 begin
   if ArrIndex = nil then
-    ArrIndex := TList.Create;
+    ArrIndex := TList<PInteger>.Create;
 
   if Vars.Count = 0 then
   begin
@@ -428,7 +429,7 @@ begin
   end;
 
   VarNo := 0;
-  while UpperCase(TVar(Vars[VarNo]^).Name) <> UpperCase(Name) do
+  while UpperCase(Vars.Items[VarNo].Name) <> UpperCase(Name) do
   begin
     Inc(VarNo);
     if VarNo = Vars.Count then
@@ -440,7 +441,7 @@ begin
     end;
   end;
 
-  _Var := TVar(Vars[VarNo]^);
+  _Var := Vars[VarNo]^;
 
   if (_Var.Value._Type = tyStr) and (_Var.Sizes.Count = 0) and (ArrIndex.Count = 1) // Обработка выбора символа строки
     then
@@ -473,7 +474,7 @@ begin
     end;
 
     for i := 0 to ArrIndex.Count - 1 do
-      if integer(ArrIndex[i]^) > integer(_Var.Sizes[i]^) - 1 then
+      if ArrIndex[i]^ > _Var.Sizes[i]^ - 1 then
       begin
         RunTimeError('Выход за пределы массива');
         Result._Type := tyReal;
@@ -487,9 +488,9 @@ begin
     for i := 0 to _Var.Sizes.Count - 1 do
     begin
       Inc(PlainNo, q * integer(ArrIndex[i]^));
-      q := q * integer(_Var.Sizes[i]^);
+      q := q * _Var.Sizes[i]^;
     end;
-    Result := TValue(_Var.Arr[PlainNo]^);
+    Result := _Var.Arr[PlainNo]^;
   end;
 end;
 
@@ -505,7 +506,7 @@ begin
   end;
 
   VarNo := 0;
-  while UpperCase(TVar(Vars[VarNo]^).Name) <> UpperCase(Name) do
+  while UpperCase(Vars.Items[VarNo].Name) <> UpperCase(Name) do
   begin
     Inc(VarNo);
     if VarNo = Vars.Count then
@@ -517,7 +518,7 @@ begin
   Result := VarNo;
 end;
 
-procedure SetVarValue(Vars: TVars; Name: string; ArrIndex: TList; Value: TValue);
+procedure SetVarValue(Vars: TVars; Name: string; ArrIndex: TList<PInteger>; Value: TValue);
 var
   i, VarNo: LongInt;
   PlainNo: Cardinal;
@@ -527,14 +528,14 @@ var
 
 begin
   if ArrIndex = nil then
-    ArrIndex := TList.Create;
+    ArrIndex := TList<PInteger>.Create;
 
   VarNo := 0;
   New(myVar);
   if Vars.Count = 0 then
     VarNo := -1
   else
-    while UpperCase(TVar(Vars[VarNo]^).Name) <> UpperCase(Name) do
+    while UpperCase(Vars.Items[VarNo].Name) <> UpperCase(Name) do
     begin
       Inc(VarNo);
       if VarNo = Vars.Count then
@@ -552,8 +553,8 @@ begin
     myVar^.Value._Type := tyReal;
     myVar^.Value.Str := '';
     myVar^.Value.Real := 0;
-    myVar^.Sizes := TList.Create;
-    myVar^.Arr := TList.Create;
+    myVar^.Sizes := TList<PInteger>.Create;
+    myVar^.Arr := TList<PValue>.Create;
     { VarNo:= } Vars.Add(myVar);
   end;
 
@@ -586,7 +587,7 @@ begin
     end;
 
     for i := 0 to ArrIndex.Count - 1 do
-      if integer(ArrIndex[i]^) > integer(myVar.Sizes[i]^) - 1 then
+      if ArrIndex[i]^ > myVar.Sizes[i]^ - 1 then
       begin
         RunTimeError('Выход за пределы массива');
         Exit;
@@ -599,7 +600,7 @@ begin
       Inc(PlainNo, q * integer(ArrIndex[i]^));
       q := q * integer(myVar.Sizes[i]^);
     end;
-    TValue(myVar.Arr[PlainNo]^) := Value;
+    myVar.Arr[PlainNo]^ := Value;
   end;
 end;
 
@@ -760,17 +761,19 @@ function Expr: TValue; forward;
   var
     Name: string;
     tmp: TValue;
-    ArrIndex: TList;
-    pInt: ^integer;
+    FuncArguments: TList<PValue>;
+    ArrIndex: TList<PInteger>;
+    pInt: PInteger;
     pVal: PValue;
 
   begin
-    if Lexemes^[Pos]._Type = lxName // Возвращаем значение переменной
-      then
+    if Lexemes^[Pos]._Type = lxName then
     begin
+      // Возвращаем значение переменной
       Name := Lexemes^[Pos].Value;
       Inc(Pos);
-      ArrIndex := TList.Create;
+      FuncArguments := TList<PValue>.Create;
+      ArrIndex := TList<PInteger>.Create;
       if Lexemes^[Pos]._Type = lxLBracket then
         if Assigned(GetFuncValue) then
         begin
@@ -779,17 +782,17 @@ function Expr: TValue; forward;
           begin
             New(pVal);
             pVal^ := Expr;
-            ArrIndex.Add(pVal);
+            FuncArguments.Add(pVal);
             while Lexemes^[Pos]._Type <> lxRBracket do
             begin
               Inc(Pos);
               New(pVal);
               pVal^ := Expr;
-              ArrIndex.Add(pVal);
+              FuncArguments.Add(pVal);
             end;
           end;
           Inc(Pos);
-          Result := GetFuncValue(Vars, Name, ArrIndex);
+          Result := GetFuncValue(Vars, Name, FuncArguments);
         end
         else
           RunTimeError('Внутренняя ошибка: не установлена процедура обработки функций')
@@ -1229,7 +1232,7 @@ procedure ExecOperator(Lexemes: PLexemes; Vars: TVars); // Обработка с
     VarNo: LongInt;
     myVar: PVar;
     tmpExpr: TValue;
-    pInt: ^integer;
+    pInt: PInteger;
     pVal: PValue;
     i, MultSizes: LongInt;
 
@@ -1241,7 +1244,7 @@ procedure ExecOperator(Lexemes: PLexemes; Vars: TVars); // Обработка с
     if Vars.Count = 0 then
       VarNo := -1
     else
-      while UpperCase(TVar(Vars[VarNo]^).Name) <> UpperCase(Name) do
+      while UpperCase(Vars.Items[VarNo].Name) <> UpperCase(Name) do
       begin
         Inc(VarNo);
         if VarNo = Vars.Count then
@@ -1262,8 +1265,8 @@ procedure ExecOperator(Lexemes: PLexemes; Vars: TVars); // Обработка с
     begin
       New(myVar);
       myVar^.Name := Name;
-      myVar^.Sizes := TList.Create;
-      myVar^.Arr := TList.Create;
+      myVar^.Sizes := TList<PInteger>.Create;
+      myVar^.Arr := TList<PValue>.Create;
       Vars.Add(myVar);
     end;
 
@@ -1310,9 +1313,9 @@ procedure ExecOperator(Lexemes: PLexemes; Vars: TVars); // Обработка с
   procedure Oper; // Обработка одного оператора
   var
     Name: string;
-    ArrIndex: TList;
+    ArrIndex: TList<PInteger>;
     Value: TValue;
-    pInt: ^integer;
+    pInt: PInteger;
     tmp: TValue;
     FirstPos: Cardinal;
 
@@ -1337,7 +1340,7 @@ procedure ExecOperator(Lexemes: PLexemes; Vars: TVars); // Обработка с
         ExecExpr(Lexemes, Vars);
         Exit;
       end;
-      ArrIndex := TList.Create;
+      ArrIndex := TList<PInteger>.Create;
       if Lexemes^[Pos]._Type = lxLSBracket then
       begin
         Inc(Pos);
@@ -1383,7 +1386,7 @@ begin
   end;
 end;
 
-function GetFuncResult(Vars: TVars; Name: string; Params: TList): TValue;
+function GetFuncResult(Vars: TVars; Name: string; Params: TList<PValue>): TValue;
 type
   FuncType = (ftMath, ftString, ftOther);
 
@@ -1557,7 +1560,7 @@ begin
   end;
 
   for j := 0 to Params.Count - 1 do
-    P[j + 1] := TValue(Params[j]^);
+    P[j + 1] := Params[j]^;
 
   case Funcs[i]._Type of
     ftMath:
@@ -1772,24 +1775,24 @@ begin
   if n = 'read' then
   begin
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
+      if Files.Items[i].ID = P[1].Real then
       begin
-        Result := MakeVal(PFile_Rec(Files[i]).Strings[PFile_Rec(Files[i]).Pos]);
-        Inc(PFile_Rec(Files[i]).Pos);
+        Result := MakeVal(Files.Items[i].Strings[Files.Items[i].Pos]);
+        Inc(Files.Items[i].Pos);
       end;
   end;
   if n = 'eof' then
   begin
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
-        Result := MakeVal(IfThen(PFile_Rec(Files[i]).Pos >= PFile_Rec(Files[i]).Strings.Count, 1, 0));
+      if Files.Items[i].ID = P[1].Real then
+        Result := MakeVal(IfThen(Files.Items[i].Pos >= Files.Items[i].Strings.Count, 1, 0));
   end;
   if n = 'write' then
   begin
     if (Params.Count < 1) or (P[1]._Type <> tyReal) then
       RunTimeError('Неправильный список параметров функции Write');
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
+      if Files.Items[i].ID = P[1].Real then
       begin
         File_Rec := Files[i];
         s := '';
@@ -1808,7 +1811,7 @@ begin
     if (Params.Count < 1) or (P[1]._Type <> tyReal) then
       RunTimeError('Неправильный список параметров функции WriteOver');
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
+      if Files.Items[i].ID = P[1].Real then
       begin
         File_Rec := Files[i];
         s := '';
@@ -1830,21 +1833,21 @@ begin
   begin
     Check(2, [tyReal, tyReal]);
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
-        PFile_Rec(Files[i]).Pos := min(Trunc(P[2].Real), PFile_Rec(Files[i]).Strings.Count);
+      if Files.Items[i].ID = P[1].Real then
+        Files.Items[i].Pos := min(Trunc(P[2].Real), Files.Items[i].Strings.Count);
     Result := MakeVal(0);
   end;
   if n = 'filesize' then
   begin
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
-        Result := MakeVal(PFile_Rec(Files[i]).Strings.Count);
+      if Files.Items[i].ID = P[1].Real then
+        Result := MakeVal(Files.Items[i].Strings.Count);
   end;
   if n = 'filepos' then
   begin
     for i := 0 to Files.Count - 1 do
-      if PFile_Rec(Files[i]).ID = P[1].Real then
-        Result := MakeVal(PFile_Rec(Files[i]).Pos);
+      if Files.Items[i].ID = P[1].Real then
+        Result := MakeVal(Files.Items[i].Pos);
   end;
 
   if n = 'pi' then

Source/Main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  Menus, Buttons, ExtCtrls, StdCtrls, EdTypes, OpenUnit, SaveUnit, Math,
+  Menus, Buttons, ExtCtrls, StdCtrls, EdTypes, OpenUnit, SaveUnit, Math, Generics.Collections,
   ComCtrls, ToolWin, ActnList, AppEvnts, ShellAPI, Arrows, JPEG, ini, StrUtils, Lang;
 
 type
@@ -215,7 +215,7 @@ var
 
   AplName: string;
 
-  UndoStack: TList;
+  UndoStack: TList<PUndoNode>;
 
 procedure AddToGlobVars(Lines: TStringList);
 procedure AutoPause;
@@ -361,12 +361,12 @@ begin
     ChildForm.Actives.Clear;
     LoadScheme(ChildForm.FileName);
     for i := 0 to ChildForm.ArrowList.Count - 1 do
-      TArrow(ChildForm.ArrowList[i]).StandWell;
+      ChildForm.ArrowList.Items[i].StandWell;
     ChildForm.Caption := OpenDialog.FileName;
     ChildForm.SetRange;
     Modifed := false;
     for i := 0 to UndoStack.Count - 1 do
-      Dispose(PUndoNode(UndoStack[i]));
+      Dispose(UndoStack[i]);
     UndoStack.Clear;
     mnuUndo.Enabled := false;
 
@@ -608,13 +608,14 @@ begin
   Min := Point(0, 0);
   if ChildForm.BlockList.Count > 0 then
   begin
-    Min.X := TBlock(ChildForm.BlockList[0]).Left;
-    Min.Y := TBlock(ChildForm.BlockList[0]).Top;
-    Max.X := TBlock(ChildForm.BlockList[0]).Left + TBlock(ChildForm.BlockList[0]).Width;
-    Max.Y := TBlock(ChildForm.BlockList[0]).Top + TBlock(ChildForm.BlockList[0]).Height;
+    b := ChildForm.BlockList[0];
+    Min.X := b.Left;
+    Min.Y := b.Top;
+    Max.X := b.Left + b.Width;
+    Max.Y := b.Top + b.Height;
     for i := 0 to ChildForm.BlockList.Count - 1 do
     begin
-      b := TBlock(ChildForm.BlockList[i]);
+      b := ChildForm.BlockList[i];
       if b.Left < Min.X then
         Min.X := b.Left;
       if b.Top < Min.Y then
@@ -626,7 +627,7 @@ begin
     end;
     for i := 0 to ChildForm.ArrowList.Count - 1 do
     begin
-      a := TArrow(ChildForm.ArrowList[i]);
+      a := ChildForm.ArrowList[i];
       for t := atStart to atEnd do
       begin
         if a.Tail[t].X < Min.X then
@@ -677,7 +678,7 @@ begin
     MainForm.SaveDialog.FileName);
   for i := 0 to ChildForm.BlockList.Count - 1 do
   begin
-    b := TBlock(ChildForm.BlockList[i]);
+    b := ChildForm.BlockList[i];
     b.DrawCanvas := MFC;
     b.XOffs := b.Left - Min.X;
     b.YOffs := b.Top - Min.Y;
@@ -688,7 +689,7 @@ begin
   end;
   for i := 0 to ChildForm.ArrowList.Count - 1 do
   begin
-    a := TArrow(ChildForm.ArrowList[i]);
+    a := ChildForm.ArrowList[i];
     a.DrawCanvas := MFC;
     a.xo := -Min.X;
     a.yo := -Min.Y;

Source/OpenUnit.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, ExtCtrls, EdTypes, Arrows;
+  StdCtrls, ExtCtrls;
 
 var
   OpenFileBlok: string;
@@ -16,7 +16,7 @@ procedure LoadScheme(FileName: string);
 
 implementation
 
-uses Main, Child;
+uses Main, Child, EdTypes, Arrows;
 
 var
   F: TextFile;
@@ -164,7 +164,7 @@ var
         if str = 'SOUTH' then
           A.Blocks[t].Port := South;
         str := DoRead;
-        A.Blocks[t].Block := TBlock(ChildForm.BlockList[StrToInt(str) + OBC]);
+        A.Blocks[t].Block := ChildForm.BlockList[StrToInt(str) + OBC];
       end;
       { str:=DoRead; }
     end;

Source/Options.pas
@@ -78,17 +78,21 @@ end;
 procedure TfrmOpt.BitOkClick(Sender: TObject);
 var
   i: integer;
+  block: TBlock;
 
 begin
   ChildForm.WidthBlok := StrToInt(WidthBlok.Text);
   ChildForm.HeightBlok := StrToInt(HeightBlok.Text);
   ChildForm.ConflRadius := StrToInt(ConflRad.Text);
   for i := 0 to ChildForm.BlockList.Count - 1 do
-    if TBlock(ChildForm.BlockList[i]).Block = stConfl then
+  begin
+    block := ChildForm.BlockList.Items[i];
+    if block.Block = stConfl then
     begin
-      TBlock(ChildForm.BlockList[i]).Width := ChildForm.ConflRadius;
-      TBlock(ChildForm.BlockList[i]).Height := ChildForm.ConflRadius;
+      block.Width := ChildForm.ConflRadius;
+      block.Height := ChildForm.ConflRadius;
     end;
+  end;
   ChildForm.Refresh;
 
   ChildForm.AutoCheck := clbInterpr.Checked[0];

commit b9b512046987f40bbc6113925cf89c982413c433

Fix bug introduced in 99a5d71e

Source/EdTypes.pas
@@ -76,6 +76,7 @@ begin
         s := false;
         e := false;
         for i := 0 to ChildForm.ArrowList.Count - 1 do
+        begin
           blockRec := ChildForm.ArrowList.Items[i].Blocks[atEnd];
           if blockRec.Block = Self then
           begin
@@ -86,6 +87,7 @@ begin
             if blockRec.Port = East then
               e := true;
           end;
+        end;
         if (w and s) or (s and e) or (e and w) then
         begin
           Result := false;