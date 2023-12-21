3bc837a1da2541ecc7d3b07dab11d83f76fedc69
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2023-12-21 16:17:32 +0200

    Preparing v3.4 distributive

--- Source/Version.pas
+++ Source/Version.pas
@@ -3,8 +3,8 @@ unit Version;
 interface
 
 const
-  BuildDate = 45269.959075706;
-  BuildNumber = 384;
+  BuildDate = 45281.6724613426;
+  BuildNumber = 385;
   BuildVersion = '3.4';
 
 implementation
